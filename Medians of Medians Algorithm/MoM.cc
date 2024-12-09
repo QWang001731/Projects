9162c5bc26438cffdac77fa3cdcf5544b4e46c19
Q Wang <eason06111@sina.com>
2024-12-08 22:16:30 -0600

    Update MoM.cc

--- Medians of Medians Algorithm/MoM.cc
+++ Medians of Medians Algorithm/MoM.cc
@@ -4,53 +4,53 @@
 #include<cmath>
 #include<cassert>
 using namespace std;
+int medianOfMedian(vector<int>&v, int k,int left, int right){
+    int n =right - left + 1;
+    int m=n/5;
+    if (5 * m < n) 
+        m++;
+    vector<vector<optional<int>>>matrix(m, vector<optional<int>>(5,nullopt));
+    vector<int>median(m);
+    for(int i=left;i<=right;i++){
+        int row = (i-left)/5;
+        int col = (i-left)%5;
+        matrix[row][col]=v[i];
+        }
 
-template<class T>
-T Median(vector<T>v){
-    sort(v.begin(),v.end());
-    return v[v.size()/2];
-}
-template<class T>
-T MoM(vector<T>v, size_t i){
-    assert(i<=v.size());
-    if (v.size()==1) return v[0];
-    int num_groups=ceil(v.size()/5.);
-    vector<vector<T>> sub_group(num_groups, vector<T>(5,0));
-    int last_group_num = v.size()-5*(num_groups-1);
-    sub_group[num_groups-1].resize(last_group_num,0);
-
-    for(int i1=0;i1<num_groups-1;i1++){
-        int start = i1*5; 
-        for(int i2=0;i2<5;i2++)
-            sub_group[i1][i2]=v[start+i2];
-    }
-
-    int start = 5*(num_groups-1);
-    for(int i1=0;i1<last_group_num;i1++)
-        sub_group[num_groups-1][i1]=v[start+i1];
+    for(int i=0;i<m;i++)
+        median[i]=median_element(matrix[i]);
+        
+    int med=median_element(median);
+    int medidx=get_idx(v,med);
     
-    vector<T> medians(num_groups, 0);
-    for(int ii=0;ii<num_groups;ii++)
-        medians[ii] = Median<T>(sub_group[ii]);
+    int t = v[medidx];
+    v[medidx]=v[right];
+    v[right]=t;
     
-    T m = MoM<T>(medians, medians.size()/2+1);
-    vector<T>v1,v2;
-    for(int j=0;j<v.size();j++){
-        if(v[j]<m) 
-            v1.push_back(v[j]);
-        else if(v[j]>m) 
-            v2.push_back(v[j]);
-    }
+    int i=left-1;
+    int x = v[right];
+    for(int j=left;j<right;j++){
+        if(v[j]<=x){
+            i++;
+            int t = v[i];
+            v[i]=v[j];
+            v[j]=t;
+            }
+        }
+    int t1 = v[i+1];
+    v[i+1]=v[right];
+    v[right]=t1;
 
-    if (v1.size()==i-1) 
-        return m;
-    else if(v1.size()>i-1) 
-        return MoM<T>(v1, i);
+    int h=n-(i-left+1);
+    if(k==h) 
+        return v[i+1];
+    else if (k>h) 
+        return medianOfMedian(v,k-h,left,i);
     else 
-        return MoM<T>(v2, i-(1+v1.size()));
+        return medianOfMedian(v,k,i+2,right);
 }
 
 int main(){
     vector<int>v{1,9,1,7,8,6,4,3,11,23,19,22,100,20000};
-    cout<<MoM(v,6);
+    cout<<medianOfMedian(v,6,0,13);
 }
